648f25b9671c77b666c963dd5b66563f65d45b03
hellzz13 <hpalenca13@gmail.com>
2023-06-28 07:44:58 -0300

    fix: deploy vercel

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -13,17 +13,21 @@ import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Context } from "@/context/AuthContexts";
+import { Typography } from "@mui/material";
 
 export default function Login() {
   const router = useRouter();
 
   const { handleLogin } = useContext(Context);
 
+  const [hasAccount, setHasAccount] = useState(true);
+
   const UserSchema = z.object({
     username: z.string().nonempty("Campo obrigatório"),
     password: z.string().nonempty("Campo obrigatório"),
+    // confirmPassword: z.string().nonempty("Campo obrigatório"),
   });
 
   type UserFormData = z.infer<typeof UserSchema>;
@@ -63,56 +67,131 @@ export default function Login() {
             />
           </Box>
 
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit(handleLogin)}
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              {...register("username")}
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Usuário"
-              name="username"
-              autoComplete="username"
-              autoFocus
-              error={!!errors.username}
-              helperText={errors.username && errors.username.message}
-            />
-            <TextField
-              {...register("password")}
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Senha"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              error={!!errors.password}
-              helperText={errors.password && errors.password.message}
-            />
-
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={() => router.push("/")}
+          {!hasAccount ? (
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit(handleLogin)}
+              sx={{ mt: 1 }}
             >
-              Entrar
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Não é cadastrado? Crie sua conta!"}
-                </Link>
+              <TextField
+                {...register("username")}
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Usuário"
+                name="username"
+                autoComplete="username"
+                autoFocus
+                error={!!errors.username}
+                helperText={errors.username && errors.username.message}
+              />
+              <TextField
+                {...register("password")}
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                error={!!errors.password}
+                helperText={errors.password && errors.password.message}
+              />
+
+              <TextField
+                // {...register("confirmPassword")}
+                margin="normal"
+                required
+                fullWidth
+                name="confirmPassword"
+                label="Confirmar Senha"
+                type="confirmPassword"
+                id="confirmPassword"
+                autoComplete="current-password"
+                error={!!errors.password}
+                helperText={errors.password && errors.password.message}
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => router.push("/")}
+              >
+                Entrar
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Typography>
+                    <span
+                      className="cursor-pointer"
+                      onClick={() => setHasAccount(false)}
+                    >
+                      {"Voltar"}
+                    </span>
+                  </Typography>
+                </Grid>
               </Grid>
-            </Grid>
-          </Box>
+            </Box>
+          ) : (
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit(handleLogin)}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                {...register("username")}
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Usuário"
+                name="username"
+                autoComplete="username"
+                autoFocus
+                error={!!errors.username}
+                helperText={errors.username && errors.username.message}
+              />
+              <TextField
+                {...register("password")}
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                error={!!errors.password}
+                helperText={errors.password && errors.password.message}
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => router.push("/")}
+              >
+                Entrar
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <span
+                    className="cursor-pointer"
+                    onClick={() => setHasAccount(true)}
+                  >
+                    <Typography>{"Não tem uma conta? Cadastre-se!"}</Typography>
+                  </span>
+                </Grid>
+              </Grid>
+            </Box>
+          )}
         </Box>
       </Paper>
     </main>
